22e0f17c383bac00604627eb381c790ee4d1c84e
Chandan Basumatary <cbasumat@in.ibm.com>
2020-01-15 17:28:01 +0530

    Add and update documentation for squares.

--- src/my_square.py
+++ src/my_square.py
@@ -1,9 +1,11 @@
 def my_square(x):
-    ''' takes a value and returns the squared value.
+    ''' Takes a value and returns the squared value.
     '''
     return (x ** 2)
 
 def my_square2(x):
+    ''' Uses the * operator to calculate square.
+    '''
     return (x * x)
 
 print(my_square(3))
